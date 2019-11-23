b783b0e3b76b123222a010324a03890002df1738
LinorDolev <32033925+LinorDolev@users.noreply.github.com>
2019-11-23 23:18:35 +0200

    Added CORS allowance

--- src/main/java/inventory/Application.java
+++ src/main/java/inventory/Application.java
@@ -14,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Application {
 
+    ///TODO CHECK INPUT IN SET INSTEAD OF IN CLASS
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/inventory/api/InventoryAPIImp.java
+++ src/main/java/inventory/api/InventoryAPIImp.java
@@ -3,11 +3,12 @@ package inventory.api;
 import inventory.logic.ItemService;
 import inventory.entities.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin("*")
 @RestController
 public class InventoryAPIImp implements InventoryAPI{
 
